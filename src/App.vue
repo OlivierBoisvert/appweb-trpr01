138c14464bfff19e2a65b4f4bacbda21f285af5b
OlivierBoisvert <olivierboisv@gmail.com>
2026-02-20 10:05:37 -0500

    Setup of the project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import GearList from "./components/GearList.vue";
+</script>
+
+<template>
+  <GearList />
+</template>
+
+<style scoped></style>
